a06e2c90a9feb4b54cd7502876007adb045b5440
舒达 <shuda.yy@alibaba-inc.com>
2016-11-19 22:55:58 +0800

    实现回退标红

--- js/index.js
+++ js/index.js
@@ -4,26 +4,73 @@
 
 const INVALID_INDEX = -1;
 
+let isLosersBracket = false;
+
 let stateArray = [];
 let currentStateIndex = INVALID_INDEX;
 
 let allPlayerIds = [];
 let groups = [];
-let winPlayerIds = new Set();
+let winPlayerIds = [];
+let lastRoundWinPlayersIds = [];
 
 class StateManager {
   push() {
-
+    let currentState = {
+      allPlayerIds: allPlayerIds.concat(),
+      groups:groups.concat(),
+      winPlayerIds:winPlayerIds.concat(),
+      lastRoundWinPlayersIds:lastRoundWinPlayersIds.concat(),
+      isLosersBracket:isLosersBracket,
+    };
+    stateArray.push(currentState);
   }
 
   previous() {
-
+    console.log('【回退】即将回退至index',currentStateIndex);
+    console.log('【回退】取出状态：',stateArray[currentStateIndex]);
+    if(stateArray[currentStateIndex] === undefined){
+      console.log('【错误】stateArray[currentStateIndex] === undefined');
+    }
+    allPlayerIds = stateArray[currentStateIndex].allPlayerIds.concat();
+    groups = stateArray[currentStateIndex].groups.concat();
+    winPlayerIds = stateArray[currentStateIndex].winPlayerIds.concat();
+    lastRoundWinPlayersIds = stateArray[currentStateIndex].lastRoundWinPlayersIds.concat();
+    isLosersBracket = stateArray[currentStateIndex].isLosersBracket;
+    currentStateIndex--;
   }
 
   next() {
+    currentStateIndex++;
+    console.log('即将压入队列index',currentStateIndex);
+    console.log('当前状态机队列长度',stateArray.length);
+    if(stateArray.length > currentStateIndex){
+      stateArray.splice(0,currentStateIndex-1);
+      this.push();
+    }else if(stateArray.length === currentStateIndex){
+      this.push();
+    }else{
+      console.log('stateArray.length < currentStateIndex,程序出错。');
+    }
 
+    // 准备下一轮需要用到的数据
+    // 判断当前是否为败者组
+    if(isLosersBracket){
+      allPlayerIds = _.union(lastRoundWinPlayersIds,winPlayerIds);
+      lastRoundWinPlayersIds = [];
+      winPlayerIds = [];
+      groups = [];
+    }else{
+      lastRoundWinPlayersIds = winPlayerIds.concat();
+      allPlayerIds = _.difference(allPlayerIds,winPlayerIds);
+      groups = [];
+      winPlayerIds = [];
+    }
+    isLosersBracket = !isLosersBracket;
   }
 }
+
+let stateManager = new StateManager();
 
 class DrawManager {
   draw() {
@@ -90,8 +137,17 @@ class InteractiveManager {
   }
 
   render() {
+    console.log('开始渲染页面：');
+    console.log('allPlayerIds',allPlayerIds);
+    console.log('groups',groups);
+    console.log('winPlayerIds',winPlayerIds);
+    console.log('lastRoundWinPlayersIds',lastRoundWinPlayersIds);
+    console.log('是否败者组',isLosersBracket);
+    $('.ids-panel').children().remove();
+    $('.group-col').children().remove();
+    $('#round-count').html(currentStateIndex+2);
     if (allPlayerIds.length === 0) {
-      console.log('allPlayerIds为空');
+      console.log('错误：allPlayerIds为空');
     }
     for (let i = 0; i < allPlayerIds.length; i++) {
       if (i % 2 === 0) {
@@ -102,7 +158,7 @@ class InteractiveManager {
     }
 
     if (groups.length === 0) {
-      console.log('groups为空');
+      console.log('错误：groups为空');
     }
     for (let i = 0; i < groups.length; i++) {
       let idsElem = ''
@@ -124,15 +180,12 @@ class InteractiveManager {
           break;
       }
     }
+    _.each($('.id-group-elem'),(elem) => {
+      if(winPlayerIds.includes(parseInt(elem.innerText))){
+        $(elem).addClass('win');
+      }
+    });
     this.bindEvent();
-  }
-
-  selected() {
-
-  }
-
-  unselected() {
-
   }
 }
 
@@ -140,7 +193,6 @@ class InteractiveManager {
   const screenHeight = $(window).height();
   $('body').height(screenHeight);
 
-  let stateManager = new StateManager();
   let interactiveManager = new InteractiveManager();
   let drawManager = new DrawManager();
 
@@ -155,8 +207,24 @@ class InteractiveManager {
 
     $('#initModal').modal('hide');
     interactiveManager.render();
+  });
 
+  $('#next-button').on('click',()=>{
+    console.log('【交互】下一步按钮触发');
+    let selectedElems = $('.id-group-elem.win');
+    winPlayerIds = [];
+    for(elem of selectedElems){
+      winPlayerIds.push(parseInt(elem.innerText));
+    }
+    stateManager.next();
+    drawManager.draw();
+    interactiveManager.render();
+  });
 
+  $('#back-button').on('click',()=>{
+    console.log('【交互】上一步按钮触发');
+    stateManager.previous();
+    interactiveManager.render();
   });
 
   $('#initModal').modal('show');
